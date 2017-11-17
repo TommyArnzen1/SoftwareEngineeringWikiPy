35d1e247bc49bf8aca45be4f4a3664b4c924ff32
Tommy Arnzen <arnzent1@mymail.nku.edu>
2017-11-17 16:55:18 -0500

    Login Feature
    Login Feature.

---
+++ wiki-master/wiki/web/database/create_database.py
@@ -0,0 +1,30 @@
+import sqlite3;
+
+
+conn = sqlite3.connect('database.db')
+cursor = conn.cursor()
+
+# cursor.execute("DROP TABLE Users")
+# cursor.execute("CREATE TABLE Users(id INTEGER PRIMARY KEY AUTOINCREMENT , name TEXT, username TEXT, password TEXT)")
+# cursor.execute(("INSERT INTO Users('name', 'username', 'password') VALUES('Tommy', 'Tommy', 'Check')"))
+
+
+query = ("SELECT * FROM Users")
+# cursor.execute(query)
+
+
+# find_user = ("SELECT * FROM users WHERE username = ? AND password = ?")
+username = 'Tommy'
+password = 'Check'
+cursor.execute(query)
+# cursor.execute(find_user, [(username), (password)])
+results = cursor.fetchall()
+
+if results:
+    for i in results:
+        print(i)
+else:
+    print("Not in database")
+
+conn.commit()
+conn.close()
